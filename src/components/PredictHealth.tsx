40073f310a64f6501c164e24b10cd737c594da58
BaconKage <152167782+BaconKage@users.noreply.github.com>
2025-11-02 21:02:51 +0530

    Update PredictHealth.tsx

--- src/components/PredictHealth.tsx
+++ src/components/PredictHealth.tsx
@@ -49,7 +49,7 @@ export default function PredictHealth() {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://fds-project-backend.onrender.com/predict", {
         // If you set a Vite proxy, change to '/predict'
         method: "POST",
         headers: { "Content-Type": "application/json" },
